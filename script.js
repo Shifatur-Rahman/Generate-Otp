ffd12f0f8eeff7051c4e8cef082c6af981ca6f87
Shifatur-Rahman <rjnovel28@gmail.com>
2024-05-19 11:08:56 +0600

    otp message add

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 
-// function tackleOTPBoxes(){
-//     const box = document.getElementById('otp-box-list-id');
-//     box.addEventListener('input', function(e){
-//            const val = e.target.value;
-//            if(isNaN(val)){
-//             target.value = "";
-//             return ;
-//            }
-
-//            const nextElements = e.target.nextElementSibling;
-//            if(nextElements){
-//             nextElements.focus();
-//            }
-//     })
-// }
-
-// tackleOTPBoxes();
-
-
-
-
-
-
-
+let generatedOTP;
+// 1.    OTP Box -> code enter and next child box focus  
 
 function OTPBoxList(){
     const otpBox = document.getElementById("otp-box-list-id");
@@ -30,19 +8,70 @@ function OTPBoxList(){
         const target = e.target;
         const value = target.value;
 
+      //  console.log(target);
+
         if(isNaN(value)){
            target.value = "";
            return;
         }
 
-        console.log(value);
+        const nextElement = target.nextElementSibling;
+        if(nextElement){
+            nextElement.focus();
+        }
 
+        validateOtp();
 
     })
 }
 
+//  2.    Generate 4 digit otp
+
+function generateOTP(){
+    const otpElement = document.getElementById("generate-otp-id");
+    generatedOTP = Math.floor(1000 + Math.random() * 9000 );
+    // console.log(generateOTP);
+    otpElement.innerHTML = `Your OTP: ${generatedOTP}`;
+}
+
+//  3. otp validation
+
+function validateOtp(){
+    let otpMessage = document.getElementById('result-id');
+    let typedNumber = "";
+    const boxListElem = document.getElementById("otp-box-list-id");
+    [...boxListElem.children].forEach((element)=>{
+        typedNumber += element.value;
+    });
+
+    console.log(generatedOTP, typedNumber);
+
+    const result = (generatedOTP === parseInt(typedNumber,10));
+
+   if(result){
+    otpMessage.innerHTML = "OTP Validate successfully";
+    otpMessage.classList.remove('fail');
+    otpMessage.classList.add('success');
+   } else{
+    otpMessage.innerHTML = "OTP is Invalid";
+    otpMessage.classList.remove('success');
+    otpMessage.classList.add('fail');
+   }
+}
+
+
+
 
 OTPBoxList();
+setTimeout(generateOTP,2000);
+
+
+
+//                    new things
+
+//   1. nextElementSibling
+//   2. focus()
+
 
 
 
