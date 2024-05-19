fc112df9cb34c411d80ee4ad4bc297c05f2b2f42
Shifatur-Rahman <rjnovel28@gmail.com>
2024-05-19 08:55:46 +0600

    otp box add

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
 
-function tackleOTPBoxes(){
-    const box = document.getElementById('otp-box-list-id');
-    box.addEventListener('input', function(e){
-           const val = e.target.value;
-           if(isNaN(val)){
-            target.value = "";
-            return ;
-           }
+// function tackleOTPBoxes(){
+//     const box = document.getElementById('otp-box-list-id');
+//     box.addEventListener('input', function(e){
+//            const val = e.target.value;
+//            if(isNaN(val)){
+//             target.value = "";
+//             return ;
+//            }
 
-           const nextElements = e.target.nextElementSibling;
-           if(nextElements){
-            nextElements.focus();
-           }
+//            const nextElements = e.target.nextElementSibling;
+//            if(nextElements){
+//             nextElements.focus();
+//            }
+//     })
+// }
+
+// tackleOTPBoxes();
+
+
+
+
+
+
+
+
+function OTPBoxList(){
+    const otpBox = document.getElementById("otp-box-list-id");
+    otpBox.addEventListener('input', function(e){
+        const target = e.target;
+        const value = target.value;
+
+        if(isNaN(value)){
+           target.value = "";
+           return;
+        }
+
+        console.log(value);
+
+
     })
 }
 
-tackleOTPBoxes();
+
+OTPBoxList();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
